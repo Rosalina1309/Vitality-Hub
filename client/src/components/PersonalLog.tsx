57f9b84a1e338177d397bf5d1e7656ad2a7a6b90
Quintensevinga <quintensevinga@outlook.com>
2023-10-09 13:36:01 +0200

    mock hardcoded personal log comp v1

---
+++ client/src/components/PersonalLog.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import styles from "@/styles/personallog.module.css";
+import { PersonalLogData } from "@/interfaces/PersonalLog";
+
+const PersonalLog: React.FC = () => {
+  const [personalLogs, setPersonalLogs] = useState<PersonalLogData[]>([]);
+
+  useEffect(() => {
+    const hardcodedData = [
+      {
+        date: "23.03.1998",
+        food: "Breakfast: Scrambled eggs, Lunch: Grilled chicken salad",
+        exercises: "Morning jog, Evening yoga",
+      },
+      {
+        date: "24.03.1998",
+        food: "Breakfast: Oatmeal, Lunch: Vegetable stir-fry",
+        exercises: "Gym workout",
+      },
+      {
+        date: "25.03.1998",
+        food: "Breakfast: Greek yogurt, Lunch: Quinoa and roasted vegetables",
+        exercises: "Cycling",
+      },
+      {
+        date: "23.03.1998",
+        food: "Breakfast: Scrambled eggs, Lunch: Grilled chicken salad",
+        exercises: "Morning jog, Evening yoga",
+      },
+      {
+        date: "24.03.1998",
+        food: "Breakfast: Oatmeal, Lunch: Vegetable stir-fry",
+        exercises: "Gym workout",
+      },
+    ];
+
+    setPersonalLogs(hardcodedData);
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.logscontainer}>
+        {personalLogs.map((log, index) => (
+          <div className={styles.singlelogcontainer} key={index}>
+            <div className={styles.logdate}>
+              <p>{log.date}</p>
+            </div>
+            <div className={styles.personallog}>
+              <div className={styles.food}>
+                <p>Food user ate:</p>
+                <p>{log.food}</p>
+              </div>
+              <div>
+                <p>Exercises done:</p>
+                <p>{log.exercises}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PersonalLog;
