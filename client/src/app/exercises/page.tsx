a426658d3e02797c66229b260fa81f1b6f4e7194
Nhung <hongnhungpham130995@gmail.com>
2023-10-10 16:57:39 +0200

    feat: making another path /exercises

---
+++ client/src/app/exercises/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CaloriesChartComponent from '@/components/CaloriesChartComponent';
+import ExercisesComponent from '@/components/ExercisesComponent';
+
+const ExercisesPage = () => {
+  return (
+    <>
+      <div>
+        <div >
+          <CaloriesChartComponent />
+        </div>
+         <div>
+          <ExercisesComponent />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ExercisesPage;
--- client/src/app/recipes/page.tsx
+++ client/src/app/recipes/page.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Recipes from '@/components/RecipesComponent';
 import styles from '@/styles/recipes-page.module.css';
 import CaloriesChartComponent from '@/components/CaloriesChartComponent';
-import CalculatorsComponent from '@/components/CalculatorsComponent';
-import ExercisesComponent from '@/components/ExercisesComponent';
-import NutritionComponent from '@/components/NutritionComponent';
+
 
 const RecipesPage = () => {
   return (
@@ -16,9 +14,6 @@ const RecipesPage = () => {
         <div className={styles.right}>
           <CaloriesChartComponent />
         </div>
-        <CalculatorsComponent />
-        <ExercisesComponent />
-        <NutritionComponent />
       </div>
     </>
   );
