2a1de5c97947222810011cd7ec39429c3817ce1d
Patricia Bárcena <45976722+Patri27@users.noreply.github.com>
2023-10-10 19:53:12 +0200

    fix: password can no longer be shorter than 6 characters

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, email: true, uniqueness: true
   validates :birthdate, presence: true, allow_blank: true
+  validates :password, presence: true, length: { minimum: 6 }
   validates :gender, inclusion: { in: %w(male female other), message: 'is not a valid gender' }, allow_blank: true
   validates :password_digest, presence: true, length: { minimum: 6 }
 
