449c615190c9f9af7e7f4c61dca3588cff4f630f
Nhung <hongnhungpham130995@gmail.com>
2023-10-11 11:09:32 +0200

    feat: working on the login component

--- client/src/apiServices/getCSRFToken.ts
+++ client/src/apiServices/getCSRFToken.ts
@@ -1,17 +1,44 @@
-import axios from "axios";
+// import axios from 'axios';
+// import cheerio from 'cheerio';
 
-const rootUrl = `http://localhost:3000/`;
+// const rootUrl = `http://localhost:3001/`;
+
+// export async function getCSRFToken(): Promise<string> {
+//   try {
+//     const response = await axios.get(rootUrl);
+//     const data = response.data;
+
+//     const $ = cheerio.load(data);
+//     const csrfToken = $('meta[name="csrf-token"]').attr('content');
+
+//     if (csrfToken) {
+//       console.log('CSRF Token:', csrfToken);
+//       return csrfToken;
+//     } else {
+//       throw new Error('CSRF Token not found in the response content.');
+//     }
+//   } catch (err) {
+//     throw err;
+//   }
+// }
+
+
+import axios from 'axios';
+
+const rootUrl = `http://localhost:3001/`;
 
 export async function getCSRFToken(): Promise<string> {
   try {
     const response = await axios.get(rootUrl);
     const data = response.data;
+
     const tokenPattern = /<meta name="csrf-token" content="(.+?)" \/>/;
     const match = data.match(tokenPattern);
+
     if (match && match[1]) {
-      const token = match[1];
-      console.log('CSRF Token:', token);
-      return token;
+      const csrfToken = match[1];
+      console.log('CSRF Token:', csrfToken);
+      return csrfToken;
     } else {
       throw new Error('CSRF Token not found in the response content.');
     }
