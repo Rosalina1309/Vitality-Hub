d21c4fd32efc1cb2ef01e1a30c7058ffad6a6b91
Riccardo Murru <riccardo.mr@gmail.com>
2023-10-11 19:59:22 +0200

    style: fix exercise style

--- client/src/pages/exercises.tsx
+++ client/src/pages/exercises.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import CaloriesChartComponent from '@/components/CaloriesChartComponent';
 import ExercisesComponent from '@/components/ExercisesComponent';
 import CalculatorsComponent from '@/components/CalculatorsComponent';
+import styles from '@/styles/exercisePage.module.css';
 
 const ExercisesPage = () => {
   return (
     <>
-      <div>
-        <div>
-          <CaloriesChartComponent />
-        </div>
-        <div>
-          <ExercisesComponent />
-          <CalculatorsComponent />
-        </div>
-      </div>
+      <main className={styles.main}>
+        <CaloriesChartComponent />
+        <ExercisesComponent />
+        <CalculatorsComponent />
+      </main>
     </>
   );
 };
