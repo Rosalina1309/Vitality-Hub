4310a4c69563aa6f5ce3da0509ca56265d5a037d
Nhung <hongnhungpham130995@gmail.com>
2023-10-12 22:22:50 +0200

    feat: ProfileInfosComponent.tsx was created and have a basic css, get all the infos and liked recipes & exercises and personal infos

--- client/src/apiServices/fetchUserInfos.ts
+++ client/src/apiServices/fetchUserInfos.ts
@@ -4,35 +4,7 @@ export async function fetchUserInfos(token: string) {
   try {
     const graphqlEndpoint = 'http://localhost:3001/graphql';
     const query = `
-      {
-        user {
-          id
-          username
-          email
-          gender
-          userMeasurements {
-            height
-            weight
-            measurementUnit
-            bmi
-            waist
-            hips
-            whr
-          }
-          userGoals {
-            id
-            personalGoal
-            startDate
-            endDate
-          }
-          favoriteExercises {
-            exerciseId
-          }
-          favoriteRecipes {
-            recipeId
-          }
-        }
-      }
+    {user {id username email birthdate gender favoriteExercises{ exercise {id name type muscle equipment difficulty instructions}}favoriteRecipes{recipe{id title image calories protein fat carbs}}}}
     `;
 
     const response = await axios.post(
--- client/src/components/ProfileInfosComponent.tsx
+++ client/src/components/ProfileInfosComponent.tsx
@@ -1,106 +1,21 @@
 
-
-// 'use client'
-
-// import { useEffect, useState } from 'react';
-// import { fetchUserInfos } from '../apiServices/fetchUserInfos';
-// import { User, UserExercise, UserRecipe } from '../interfaces/UserInterfaces';
-// import styles from '../styles/UserProfile.module.css';
-
-// const UserProfile = () => {
-//   const [user, setUser] = useState<User | null>(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         // Retrieve the token from localStorage
-//         const token = localStorage.getItem('token');
-//         console.log(token)
-//         if (token) {
-//           const userData = await fetchUserInfos(token);
-//           console.log('userData: ', userData)
-//           console.log(userData.favoriteRecipes)
-//           setUser(userData);
-//         } else {
-//           console.error('Token not found');
-//         }
-//       } catch (error) {
-//         console.error('Error fetching user data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   function findRecipesById () {
-
-//   }
-
-//   return (
-//     <div className={styles.userProfile}>
-//       {user ? (
-//         <>
-//           <h1>Welcome, {user.username}!</h1>
-//           <div className={styles.userInfo}>
-//             <h2>User Information</h2>
-//             <p>Email: {user.email}</p>
-//             <p>Gender: {user.gender}</p>
-//           </div>
-//           <div className={styles.favoriteExercises}>
-//             <h4>Favorite Exercises</h4>
-//             <ul>
-//               {user.favoriteExercises && user.favoriteExercises.map((exercise: UserExercise) => (
-//                 <li key={exercise.exerciseId}>{exercise.exerciseId}</li>
-//               ))}
-//             </ul>
-//           </div>
-//           <div className={styles.favoriteRecipes}>
-//             <h4>Favorite Recipes</h4>
-//             <ul>
-//               {user.favoriteRecipes && user.favoriteRecipes.map((recipe: UserRecipe) => (
-//                 <li key={recipe.recipeId}>{recipe.recipeId}</li>
-//               ))}
-//             </ul>
-//           </div>
-//         </>
-//       ) : (
-//         <p>Loading user data...</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default UserProfile;
-
 'use client'
-import { useEffect, useState } from 'react';
-import { fetchRecipes } from '../apiServices/fetchRecipes';
-import { Recipe } from '@/interfaces/Recipe';
-import { User, UserExercise, UserRecipe  } from '../interfaces/UserInterfaces';
-import styles from '../styles/UserProfile.module.css';
-import { fetchUserInfos } from '@/apiServices/fetchUserInfos';
-import { fetchExercises } from '@/apiServices/fetchExercises';
 
-const UserProfile = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
+import React, { useEffect, useState } from 'react';
+import { fetchUserInfos } from '../apiServices/fetchUserInfos';
+import { User } from '../interfaces/User';
+import styles from '../styles/userProfile.module.css';
+
+const ProfileInfosComponent: React.FC = () => {
+  const [user, setUser] = useState<User>();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-      
         const token = localStorage.getItem('token');
-
         if (token) {
-         
           const userData = await fetchUserInfos(token);
           setUser(userData);
-
-          const allRecipes = await fetchRecipes();
-          setRecipes(allRecipes);
-
-          const allExercises = await fetchExercises();
-
         } else {
           console.error('Token not found');
         }
@@ -112,54 +27,48 @@ const UserProfile = () => {
     fetchData();
   }, []);
 
-  function findRecipeById(recipeId: string): Recipe | undefined {
-    return recipes.find((recipe) => recipe.id === recipeId);
-  }
-
   return (
     <div className={styles.userProfile}>
-    {user ? (
-      <>
-        <h1>Welcome, {user.username}!</h1>
-        <div className={styles.userInfo}>
-          <h2>User Information</h2>
-          <p>Email: {user.email}</p>
-          <p>Gender: {user.gender}</p>
-        </div>
-        <div className={styles.favoriteExercises}>
-          <h4>Favorite Exercises</h4>
-          <ul>
-            {user.favoriteExercises &&
-              user.favoriteExercises.map((exercise: UserExercise) => (
-                <li key={exercise.exerciseId}>{exercise.exerciseId}</li>
-              ))}
-          </ul>
-        </div>
-        <div className={styles.favoriteRecipes}>
-          <h4>Favorite Recipes</h4>
-          <ul>
-            {user.favoriteRecipes &&
-              user.favoriteRecipes.map((recipe: UserRecipe) => {
-                const foundRecipe = findRecipeById(recipe.recipeId);
-                return foundRecipe ? (
-                  <li key={recipe.recipeId}>
-                  <img src={foundRecipe.image} alt={foundRecipe.title} className={styles.recipeImage} />
-                  <div>
-                    <p>{foundRecipe.title}</p>
-                    <p>Calories: {foundRecipe.calories}</p>
-                  </div>
-                </li>
-                ) : null;
-              })}
-          </ul>
-        </div>
-      </>
-    ) : (
-      <p>Loading user data...</p>
-    )}
-  </div>
+      {user ? (
+        <>
+          <h1>Welcome, {user.username}!</h1>
+          <div className={styles.userInfo}>
+            <h2>User Informations:</h2>
+            <p>Email: {user.email}</p>
+            <p>Gender: {user.gender}</p>
+          </div>
+          <div className={styles.favoriteExercises}>
+            <h3>Favorite Exercises</h3>
+            <ul>
+              {user.favoriteExercises &&
+                user.favoriteExercises.map((favExercise: any) => (
+                  <li key={favExercise.exercise.id}>
+                    <strong>{favExercise.exercise.name}</strong> - {favExercise.exercise.instructions}
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <div className={styles.favoriteRecipes}>
+            <h3>Favorite Recipes</h3>
+            <ul>
+              {user.favoriteRecipes &&
+                user.favoriteRecipes.map((favRecipe: any) => (
+                  <li key={favRecipe.recipe.id}>
+                    <img src={favRecipe.recipe.image} alt={favRecipe.recipe.title} className={styles.recipeImage} />
+                    <div>
+                      <p>{favRecipe.recipe.title}</p>
+                      <p>Calories: {favRecipe.recipe.calories}</p>
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <p>Loading user data...</p>
+      )}
+    </div>
   );
 };
 
-export default UserProfile;
-
+export default ProfileInfosComponent;
