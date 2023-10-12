2815b36cdde21e691700b69d5eeabd18b2b4bce8
Nhung <hongnhungpham130995@gmail.com>
2023-10-12 23:12:02 +0200

    feat: make the add to favourite worsk with both exercises and recipes

--- client/src/components/ExercisesComponent.tsx
+++ client/src/components/ExercisesComponent.tsx
@@ -1,77 +1,5 @@
 
-// 'use client'
-// import React, { useState, useEffect } from 'react';
-// import { fetchExercises } from "@/apiServices/fetchExercises";
-// import { Exercise } from '@/interfaces/Exercise';
-// import styles from '../styles/exercisesComponent.module.css';
-
-// const ExercisesComponent: React.FC = () => {
-//   const [muscle, setMuscle] = useState<string>('');
-//   const [exercises, setExercises] = useState<Exercise[] | undefined>(undefined);
-//   const [selectedExercise, setSelectedExercise] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const data = await fetchExercises(muscle);
-//         // Limit the exercises to 5
-//         setExercises(data.slice(0, 5));
-//       } catch (err) {
-//         console.error("Error fetching exercises:", err);
-//       }
-//     };
-
-//     fetchData();
-
-//   }, [muscle]);
-
-//   const handleExerciseClick = (exerciseName: string) => {
-//     setSelectedExercise(exerciseName === selectedExercise ? null : exerciseName);
-//   };
-
-//   return (
-//     <div className={styles.container}>
-//       <h2>Enter Muscle:</h2>
-//       <div className={styles.inputwrapper}>
-//         <input
-//           type="text"
-//           value={muscle}
-//           onChange={(e) => setMuscle(e.target.value)}
-//         />
-//       </div>
-//       <div>
-//   {exercises !== undefined && exercises.length > 0 ? (
-//     exercises.map((exercise) => (
-//       <div key={exercise.name} className={styles['exercise-card']}>
-//         <h3>{exercise.name}</h3>
-//         <p><strong>Type:</strong> {exercise.type}</p>
-//         <p><strong>Muscle:</strong> {exercise.muscle}</p>
-//         <p><strong>Equipment:</strong> {exercise.equipment}</p>
-//         <p><strong>Difficulty:</strong> {exercise.difficulty}</p>
-//         <button onClick={() => handleExerciseClick(exercise.name)}>
-//           {selectedExercise === exercise.name ? "Hide" : "Show"} Instructions
-//         </button>
-//         {selectedExercise === exercise.name && (
-//           <>
-//             <h4><strong>Instructions:</strong></h4>
-//             <p>{exercise.instructions}</p>
-//           </>
-//         )}
-//       </div>
-//     ))
-//   ) : (
-//     <p className={styles['error-message']}>Loading...</p>
-//   )}
-// </div>
-
-//     </div>
-//   );
-// };
-
-// export default ExercisesComponent;
-
-
-'use client'
+'use client';
 
 import React, { useState, useEffect } from 'react';
 import { fetchExercises } from "@/apiServices/fetchExercises";
@@ -85,6 +13,7 @@ const ExercisesComponent: React.FC = () => {
   const [favoriteExercises, setFavoriteExercises] = useState<string[]>([]);
 
   useEffect(() => {
+    const token = localStorage.getItem('token');
     const savedFavorites = localStorage.getItem('favoriteExercises');
     if (savedFavorites) {
       setFavoriteExercises(JSON.parse(savedFavorites));
@@ -107,55 +36,75 @@ const ExercisesComponent: React.FC = () => {
     setSelectedExercise(exerciseName === selectedExercise ? null : exerciseName);
   };
 
-  const handleToggleFavorite = (exerciseName: string) => {
-    if (favoriteExercises.includes(exerciseName)) {
-      const updatedFavorites = favoriteExercises.filter((name) => name !== exerciseName);
+  const handleToggleFavorite = async (exerciseId: string) => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:3001/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          query: `mutation ToggleFavoriteExercise {
+            toggleFavorite(type: "exercise", itemId: "${exerciseId}") {
+              user {
+                favoriteExercises {
+                  exerciseId
+                }
+              }
+            }
+          }`
+        })
+      });
+
+      const responseData = await response.json();
+      const updatedFavorites = responseData.data.toggleFavorite.user.favoriteExercises.map((fav: { exerciseId: string }) => fav.exerciseId);
       setFavoriteExercises(updatedFavorites);
       localStorage.setItem('favoriteExercises', JSON.stringify(updatedFavorites));
-    } else {
-      setFavoriteExercises([...favoriteExercises, exerciseName]);
-      localStorage.setItem('favoriteExercises', JSON.stringify([...favoriteExercises, exerciseName]));
+    } catch (error) {
+      console.error("Error toggling favorite:", error);
     }
   };
 
   return (
     <div className={styles.container}>
-      <h2>Enter Muscle:</h2>
-      <div className={styles.inputwrapper}>
-        <input
-          type="text"
-          value={muscle}
-          onChange={(e) => setMuscle(e.target.value)}
-        />
-      </div>
-      <div>
-        {exercises !== undefined && exercises.length > 0 ? (
-          exercises.map((exercise) => (
-            <div key={exercise.name} className={styles['exercise-card']}>
-              <h3>{exercise.name}</h3>
-              <p><strong>Type:</strong> {exercise.type}</p>
-              <p><strong>Muscle:</strong> {exercise.muscle}</p>
-              <p><strong>Equipment:</strong> {exercise.equipment}</p>
-              <p><strong>Difficulty:</strong> {exercise.difficulty}</p>
-              <button onClick={() => handleExerciseClick(exercise.name)}>
-                {selectedExercise === exercise.name ? "Hide" : "Show"} Instructions
-              </button>
-              <button onClick={() => handleToggleFavorite(exercise.name)}>
-                {favoriteExercises.includes(exercise.name) ? "Remove from Favorites" : "Add to Favorites"}
-              </button>
-              {selectedExercise === exercise.name && (
-                <>
-                  <h4><strong>Instructions:</strong></h4>
-                  <p>{exercise.instructions}</p>
-                </>
-              )}
-            </div>
-          ))
-        ) : (
-          <p className={styles['error-message']}>Loading...</p>
-        )}
-      </div>
+    <h2>Enter Muscle:</h2>
+    <div className={styles.inputwrapper}>
+      <input
+        type="text"
+        value={muscle}
+        onChange={(e) => setMuscle(e.target.value)}
+      />
     </div>
+    <div>
+      {exercises !== undefined && exercises.length > 0 ? (
+        exercises.map((exercise) => (
+          <div key={exercise.id} className={styles['exercise-card']}>
+            <h3>{exercise.name}</h3>
+            <p><strong>Type:</strong> {exercise.type}</p>
+            <p><strong>Muscle:</strong> {exercise.muscle}</p>
+            <p><strong>Equipment:</strong> {exercise.equipment}</p>
+            <p><strong>Difficulty:</strong> {exercise.difficulty}</p>
+            <button onClick={() => handleExerciseClick(exercise.name)}>
+              {selectedExercise === exercise.name ? "Hide" : "Show"} Instructions
+            </button>
+            <button onClick={() => handleToggleFavorite(exercise.id)}>
+              {favoriteExercises.includes(exercise.id) ? "Remove from Favorites" : "Add to Favorites"}
+            </button>
+            {selectedExercise === exercise.name && (
+              <>
+                <h4><strong>Instructions:</strong></h4>
+                <p>{exercise.instructions}</p>
+              </>
+            )}
+          </div>
+        ))
+      ) : (
+        <p className={styles['error-message']}>Loading...</p>
+      )}
+    </div>
+  </div>
   );
 };
 
--- client/src/components/RecipesComponent.tsx
+++ client/src/components/RecipesComponent.tsx
@@ -1,62 +1,22 @@
-// 'use client'
 
-// import { fetchRecipes } from "@/apiServices/fetchRecipes";
-// import { Recipe } from "@/interfaces/Recipe";
-// import React, { useState, useEffect } from "react";
-// import styles from '../styles/recipesComponent.module.css'
+'use client';
 
-// const RecipesComponent: React.FC = () => {
-//   const [recipes, setRecipes] = useState<Recipe[]>();
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const data = await fetchRecipes();
-//         setRecipes(data);
-//       } catch (error) {
-//         console.error("Error fetching recipes:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-//   return (
-//     <div className={styles.recipesContainer}>
-//       <h1>Recipes</h1>
-//       {recipes &&
-//         recipes.map((recipe) => (
-//           <div className={styles.recipeBox} key={recipe.id}>
-//             <h2>{recipe.title}</h2>
-//             <img src={recipe.image} alt={recipe.title} className={styles.recipeImage} />
-//             <p>Calories: {recipe.calories}</p>
-//             <p>Protein: {recipe.protein}</p>
-//             <p>Fat: {recipe.fat}</p>
-//             <p>Carbs: {recipe.carbs}</p>
-//             <button>Today's Favourite</button>
-//           </div>
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default RecipesComponent;
-
-'use client'
+import React, { useState, useEffect } from 'react';
 import { fetchRecipes } from "@/apiServices/fetchRecipes";
 import { Recipe } from "@/interfaces/Recipe";
-import React, { useState, useEffect } from "react";
-import styles from '../styles/recipesComponent.module.css'
+import styles from '../styles/recipesComponent.module.css';
 
 const RecipesComponent: React.FC = () => {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [favorites, setFavorites] = useState<string[]>([]);
+  const [token, setToken] = useState<string | null>(localStorage.getItem('token') || null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await fetchRecipes();
         setRecipes(data);
-        console.log(data)
+        console.log(data);
       } catch (error) {
         console.error("Error fetching recipes:", error);
       }
@@ -65,18 +25,33 @@ const RecipesComponent: React.FC = () => {
     fetchData();
   }, []);
 
-  const toggleFavorite = (recipeId: string) => {
-    if (favorites.includes(recipeId)) {
-      // If recipe is already in favorites, remove it
-      const updatedFavorites = favorites.filter(id => id !== recipeId);
+  const toggleFavorite = async (recipeId: string) => {
+    try {
+      const response = await fetch('http://localhost:3001/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          query: `mutation ToggleFavoriteRecipe {
+            toggleFavorite(type: "recipe", itemId: "${recipeId}") {
+              user {
+                favoriteRecipes {
+                  recipeId
+                }
+              }
+            }
+          }`
+        })
+      });
+
+      const responseData = await response.json();
+      const updatedFavorites = responseData.data.toggleFavorite.user.favoriteRecipes.map((fav: { recipeId: string }) => fav.recipeId);
       setFavorites(updatedFavorites);
       localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-    } else {
-      // If recipe is not in favorites, add it
-      const updatedFavorites = [...favorites, recipeId];
-      setFavorites(updatedFavorites);
-      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-      console.log(updatedFavorites)
+    } catch (error) {
+      console.error("Error toggling favorite:", error);
     }
   };
 
