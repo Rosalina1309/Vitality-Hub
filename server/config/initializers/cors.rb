2981959d52c98bffeb0984a192b31c5cd9074f03
Nhung <hongnhungpham130995@gmail.com>
2023-10-18 17:26:06 +0200

    deployment: done

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
     resource '*',
       headers:  ['Authorization', 'Content-Type', 'X-CSRF-Token'],
       methods: [:post]
