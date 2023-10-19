23fd8648533d95b29e885b25fd749e2269d92778
Patricia Bárcena <45976722+Patri27@users.noreply.github.com>
2023-10-19 11:05:33 +0200

    try24

--- server/app/lib/helpers/jwt_helper.rb
+++ server/app/lib/helpers/jwt_helper.rb
@@ -3,7 +3,7 @@ require 'jwt'
 module Helpers
   module JwtHelper
     def self.generate_token(user)
-      secret_key = ENV['SECRET_KEY']
+      secret_key = '55CEC3419F4737A4F2F99D952C962'
       payload = {
         user_id: user.id,
         username: user.username,
@@ -15,7 +15,7 @@ module Helpers
     def self.verify_jwt_token(token)
       return nil if token.blank?
 
-      secret_key = ENV['SECRET_KEY']
+      secret_key = '55CEC3419F4737A4F2F99D952C962'
       decoded_token = JWT.decode(token, secret_key, true, algorithm: 'HS256')
       user_id = decoded_token[0]['user_id']
       return user_id
