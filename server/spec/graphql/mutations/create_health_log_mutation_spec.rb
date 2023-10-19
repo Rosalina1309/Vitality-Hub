d6f645b729269d1b150a98e7b982798817b0c0d8
Patricia Bárcena <45976722+Patri27@users.noreply.github.com>
2023-10-19 10:55:31 +0200

    try22

--- server/spec/graphql/mutations/create_health_log_mutation_spec.rb
+++ server/spec/graphql/mutations/create_health_log_mutation_spec.rb
@@ -3,11 +3,11 @@ require './app/lib/helpers/jwt_helper'
 
 RSpec.describe 'Mutations::CreateHealthLogMutation', type: :request do
   it 'adds a recipe to the health log' do
-    user = FactoryBot.build(:user)
+    user = FactoryBot.create(:user)
     recipe = FactoryBot.build(:recipe)
     valid_jwt_token = Helpers::JwtHelper.generate_token(user)
 
-    allow(User).to receive(:find_by).and_return(user)
+    # allow(User).to receive(:find_by).and_return(user)
     allow(Recipe).to receive(:find).and_return(recipe)
 
 
