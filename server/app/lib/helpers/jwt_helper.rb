3fd01d8442dcc43685328eaff2d4bd321148fb73
Patricia Bárcena <45976722+Patri27@users.noreply.github.com>
2023-10-19 10:37:32 +0200

    try18

--- server/app/lib/helpers/jwt_helper.rb
+++ server/app/lib/helpers/jwt_helper.rb
@@ -9,14 +9,14 @@ module Helpers
         username: user.username,
       }
 
-      JWT.encode(payload, secret_key, ENV['JWT_ALG'])
+      JWT.encode(payload, secret_key, 'HS256')
     end
 
     def self.verify_jwt_token(token)
       return nil if token.blank?
 
       secret_key = ENV['SECRET_KEY']
-      decoded_token = JWT.decode(token, secret_key, true, algorithm: ENV['JWT_ALG'])
+      decoded_token = JWT.decode(token, secret_key, true, algorithm: 'HS256')
       user_id = decoded_token[0]['user_id']
       return user_id
     rescue JWT::DecodeError
